3513a69469baa4d557bf509e001fb69332188a66
jonsnow47 <105362036+jonsnow47@users.noreply.github.com>
2022-06-26 18:50:17 +0530

    Update Pikachu.jsx

--- pikaAPI/Pikachu.jsx
+++ pikaAPI/Pikachu.jsx
@@ -8,6 +8,13 @@ const App = ()=>{
   const [num, setNum]= useState();
   const [name, setName]= useState();
   const [move, setMove]= useState();
+  const [move1, setMove1]= useState();
+  const [move2, setMove2]= useState();
+  const [move3, setMove3]= useState();
+  const [exp, setExp]= useState();
+  const [weight, setWeight]= useState();
+  const [ability, setAbility]= useState();
+  const [ability1, setAbility1]= useState();
 
   useEffect(()=> {
     async function getData(){
@@ -15,6 +22,13 @@ const App = ()=>{
       console.log(res.data.name);
       setName(res.data.name);
       setMove(res.data.moves.length);
+      setMove1(res.data.moves[0].move.name);
+      setMove2(res.data.moves[1].move.name);
+      setMove3(res.data.moves[2].move.name);
+      setAbility(res.data.abilities[0].ability.name);
+      setAbility1(res.data.abilities[1].ability.name);
+      setWeight(res.data.weight);
+      setExp(res.data.base_experience);
     }
     getData();
   });
@@ -22,15 +36,15 @@ const App = ()=>{
   return (
    
     <>
-       <h3> You chose this : <span style={{border:'solid 2px green'}}>{num} </span></h3>
-       <h3> My name is : <span style={{border:'solid 2px blue'}}>{name} </span> </h3>
-       <h3> I have total moves numbering : <span style={{border:'solid 2px orange'}}>{move} </span> </h3>
-       
-      <select value={event} onChange={(event)=> {
+      <h1> Pokemon Go</h1>
+
+      <div className='input'> Select a number from 1 to 25 : 
+       <select value={event} onChange={(event)=> {
       setNum(event.target.value);
       }}>
+        <option value="0"></option>
         <option value="1">1</option>
-        <option value="2">2</option>      
+        <option value="2">2</option>            
         <option value="3">3</option>
         <option value="4">4</option>
         <option value="5">5</option>
@@ -55,6 +69,18 @@ const App = ()=>{
         <option value="24">24</option>
         <option value="25">25</option>
       </select>
+      </div>
+      <div className='container'>
+       <h3> You chose : <span >{num} </span></h3>
+       <h3> My name is : <span>{name} </span> </h3>
+       <h3> And I weigh : <span>{weight} kilograms</span> </h3>
+       <h3> My base experience is : <span>{exp} </span> </h3>
+       <h3> My abilities are : <span>{ability} , {ability1} </span> </h3>
+       <h3> I have total moves numbering : <span>{move} </span> </h3> <h3> and some of them are : <span>{move1}, {move2}, {move3}</span> </h3>
+        </div>
+       <br></br>  <br></br>
+      Thanks for Using Poke fan :)
+     
     </>
   );
 };
